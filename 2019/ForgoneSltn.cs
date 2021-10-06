f8f73212a80a1ea617eb04146025e52fbc91395c
Brabados <rhysipod@gmail.com>
2021-10-06 16:50:49 +1100

    First test. Using integers instead of longs or floats to makes sure the fundimentals are working and there is no decimal errors in what I'm attempting to do.

--- 2019/ForgoneSltn.cs
+++ 2019/ForgoneSltn.cs
@@ -14,10 +14,10 @@ public class Forgone
         char[] SecondNumber = new char[1];
 
         Random r = new Random();
-
+        int iteration = 0;
         foreach (char x in CharNumbers)
         {
-            int iteration = 0;
+            
 
             int ModNumber = (int)char.GetNumericValue(x);
             int NumberA;
@@ -44,21 +44,47 @@ public class Forgone
                 NumberB = ModNumber - NumberA;
             }
 
-            FirstNumber[iteration] = (char)NumberA;
-            SecondNumber[iteration] = (char)NumberB;
+            string convertA = NumberA.ToString();
+            string convertB = NumberB.ToString();
+
+            FirstNumber[iteration] = convertA[0];
+            SecondNumber[iteration] = convertB[0];
 
             Array.Resize(ref FirstNumber, FirstNumber.Length + 1);
-            Array.Resize(ref SecondNumber, FirstNumber.Length + 1);
+            Array.Resize(ref SecondNumber, SecondNumber.Length + 1);
             iteration = iteration + 1;
         }
+        iteration = 0;
 
-        
+
 
         int[] AdditiveNumbers = new int[2];
 
-        AdditiveNumbers[0] = Convert.ToInt32(FirstNumber.ToString());
-        AdditiveNumbers[1] = Convert.ToInt32(FirstNumber.ToString());
+       string test1 = "";
+       string test2 = "";
 
+        foreach (char x in FirstNumber)
+        {
+            if (iteration != FirstNumber.Length - 1)
+            {
+                test1 += (int)char.GetNumericValue(x);
+            }
+            iteration = iteration + 1;
+        }
+
+        iteration = 0;
+
+        foreach (char x in SecondNumber)
+        {
+            if (iteration != SecondNumber.Length - 1)
+            {
+                test2 += (int)char.GetNumericValue(x);
+            }
+            iteration = iteration + 1;
+        }
+
+        AdditiveNumbers[0] = Convert.ToInt32(test1);
+        AdditiveNumbers[1] = Convert.ToInt32(test2);
 
         return AdditiveNumbers;
     }
---
+++ 2019/ForgoneSltnTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var Forgone = new Forgone();
+
+        int[] result = Forgone.Input(78149);
+
+        if (result[0] + result[1] != 78149)
+            throw new InvalidOperationException();
+    }
+}
