b3223b0d701ba9f252a0972392139889ed571314
Brabados <rhysipod@gmail.com>
2021-10-08 01:50:53 +1100

    Code clean up. Set up for next exersize.

---
+++ 2015/StandingOvation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+    public class StandingOvation
+    {
+        public String NumberOfFriends(String Audience)
+        {
+
+        char[] Breakdown = Audience.ToCharArray();
+
+        int TotalShy = Breakdown.Length - 1;
+        int MinInvite = 0;
+
+        foreach(char x in Breakdown)
+        {
+
+        }
+
+        String Answer = "";
+
+        return Answer;
+        }
+
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace RhysBuszkoGoogleCodeJam
+﻿namespace RhysBuszkoGoogleCodeJam
 {
     class Program
     {
