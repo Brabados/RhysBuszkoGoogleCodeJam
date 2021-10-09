aaa226c6889e3683c983c04e2652fcb86efeb078
Brabados <33203597+Brabados@users.noreply.github.com>
2021-10-10 09:11:21 +1100

    Got Unit Test for StandingOvation integrated. Still need to check some more use case scenarios. Should be 100% working.

--- 2015/StandingOvation.cs
+++ 2015/StandingOvation.cs
@@ -2,30 +2,42 @@
 
     public class StandingOvation
     {
-        public String NumberOfFriends(String Audience)
-        {
+    public String NumberOfFriends(String[] Audiences)
+    {
+        String Answer = "";
+        int CaseNumber = 1;
 
-        char[] Breakdown = Audience.ToCharArray();
+        foreach(string ShyGroup in Audiences)
+            {
+            char[] Breakdown = ShyGroup.ToCharArray();
 
 
+            Answer += "Case " + CaseNumber.ToString() + " : " +  Calculate(Breakdown).ToString() + Environment.NewLine;
+        }
+        
+
+        return Answer;
+    }
+
+    public int Calculate(Char[] ToMod)
+    {
         int MinInvite = 0;
-            int Iterator = 0;
-            int Total = 0;
+        int Iterator = 0;
+        int Total = 0;
 
-        foreach(char x in Breakdown)
+        foreach (char x in ToMod)
         {
 
             MinInvite = Math.Max(MinInvite, Iterator - Total);
 
             Total += (int)char.GetNumericValue(x); ;
 
-            Iterator = Iterator + 1;
+            Iterator += 1;
         }
-
-        String Answer = MinInvite.ToString();
-
-        return Answer;
-        }
+        return MinInvite;
+    }
 
     }
+
+
 
---
+++ 2015/StandingTest/StandingTest/StandOvationTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using RhysBuszkoGoogleCodeJam;
+
+[TestClass]
+    public class StandOvationTest
+    {
+        [TestMethod]
+        public void CheckACorrctAnswerIsObtained()
+        {
+
+            string[] TestData = new string[1] {"11111" };
+            string Answer = "Case 1 : 0" + Environment.NewLine;
+
+            var StandingOvation = new StandingOvation();
+
+            string Result = StandingOvation.NumberOfFriends(TestData);
+
+            Assert.AreEqual(Result, Answer, Answer);
+
+        }
+
+
+        [TestMethod]
+        public void CheckMultiCalculationsForCorrectAnswers()
+        {
+            string[] TestData = new string[4] { "11111","09","110011","1" };
+            int[] Answers = new int[4] {0,1,2,0 };
+
+            bool Answer = true;
+            int Iterator = 0;
+            int Fails = 0;
+      
+            
+
+            var StandingOvation = new StandingOvation();
+
+            foreach (string x in TestData)
+            {
+            char[] Breakdown = x.ToCharArray();
+
+            if (Answers[Iterator] != StandingOvation.Calculate(Breakdown))
+                {
+                Answer = false;
+                Fails += 1;
+                }
+            Iterator += 1;
+            }
+
+        Assert.IsTrue(Answer,Fails.ToString());
+        }
+
+    }
